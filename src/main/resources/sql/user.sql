8d2248f7ebfe2e3499596189763115d77b00dffc
liuxiang <905364660@qq.com>
2020-07-27 17:50:22 +0800

    完成用户登录注册功能&员工CRUD

--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `user` (
   `id` int(32) primary key not null auto_increment,
-  `user_name` varchar(255) not null,
+  `username` varchar(255) not null,
   `password` varchar(255) not null,
   `email` varchar(255) default null,
-  `creata_time` timestamp not null default current_timestamp
-) engine=InnoDB default charset=utf8;
+  `creatatime` timestamp not null default current_timestamp
+) engine=InnoDB default charset=utf8 COMMENT='用户表';
