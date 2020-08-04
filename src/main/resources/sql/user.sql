be300cdeede390ac951644d0537d82ae9f548f32
Liu Xiang <905364660@qq.com>
2020-08-04 23:00:18 +0800

    sql文件加备注

--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `user` (
-  `id` int(32) primary key not null auto_increment,
-  `username` varchar(255) not null,
-  `password` varchar(255) not null,
-  `email` varchar(255) default null,
-  `creatatime` timestamp not null default current_timestamp
+  `id` int(32) primary key not null AUTO_INCREMENT COMMENT '用户id'
+  `username` varchar(255) not null COMMENT '用户名',
+  `password` varchar(255) not null COMMENT '密码',
+  `email` varchar(255) default null COMMENT '邮箱',
+  `creatatime` timestamp not null default current_timestamp COMMENT '创建时间'
 ) engine=InnoDB default charset=utf8 COMMENT='用户表';
